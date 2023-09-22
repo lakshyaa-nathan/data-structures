f508349b2db321735253c06feb48d685dd96786a
lakshyaa-nathan <111480891+lakshyaa-nathan@users.noreply.github.com>
2023-09-22 12:45:32 -0500

    finished pair program

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 
 /**
@@ -12,11 +14,22 @@ public class Sieve
         System.out.println("Compute primes up to which integer?");
         int n = in.nextInt();
 
-        Set <Integer> numbers =  new TreeSet <> ();
+        Set<Integer> numbers =  new HashSet<>();
 
+        for(int i = 1; i<=n; i++){
+            numbers.add(i);
 
+        }
+        
+        for(int j=3; j<=n;j++){
+            for(int k=2; k<n; k++){
+                if(j%k==0 && j!=k){
+                    numbers.remove(j);
+                }
+            }
+        }
 
-
+        System.out.println(numbers);
 
 
 
