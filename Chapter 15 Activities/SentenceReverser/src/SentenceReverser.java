01a482f2365e3db1a2578cb70c3490d04d77ad64
lakshyaa-nathan <111480891+lakshyaa-nathan@users.noreply.github.com>
2023-10-06 12:29:31 -0500

    work

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -15,14 +15,24 @@ public class SentenceReverser
     public static String reverse(String sentence)
     {
     	Scanner scanner = new Scanner(sentence);
+        
     	
         // Complete this method. Use a Stack.
-        ...
+        Stack<String> words = new Stack<>();
+        String temp;
+
+        while (sentence.length() > 0){
+            int index = sentence.indexOf(".");
+            temp = sentence.substring(0, index);
+           
 
 
 
 
 
+
+
+    }
 
     }
 }
