5a0c1840f5f6a1f9c627b14164908c9f23e2543b
lakshyaa-nathan <111480891+lakshyaa-nathan@users.noreply.github.com>
2023-10-09 13:19:05 -0500

    reversers

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -14,25 +14,63 @@ public class SentenceReverser
     */
     public static String reverse(String sentence)
     {
-    	Scanner scanner = new Scanner(sentence);
         
+    	//Scanner scanner = new Scanner(sentence);
+        String word;
+        int sentencecount = 0;
+        for(int i=0; i<sentence.length();i++){
+            if(sentence.charAt(i)=='.'){
+                sentencecount++; // counts the number of periods AKA the number of sentences
+            }
+            //System.out.println("test" + sentencecount);
+        }
     	
         // Complete this method. Use a Stack.
-        Stack<String> words = new Stack<>();
-        String temp;
+        Stack<String> words = new Stack<>(); // makes a stack for the reversed sentence
+        String temp;  // variable temporarily holding one sentence at a time to reverse
+        //Scanner tempScan;
 
-        while (sentence.length() > 0){
-            int index = sentence.indexOf(".");
+        while (sentencecount>0){  // goes until all the sentences have been reversed
+            
+            int index = sentence.indexOf("."); //finds the end of the first sentence
+
+            /* 
             temp = sentence.substring(0, index);
-           
+            // extracting the first sentence
+            
+            int length = temp.length(); // variable holding  the length of the first sentence
+
+            
 
 
+            for(int i = 0; i<length;i++) //goes until the end of the sentence
+            {
+                if(temp.charAt(i)==' '){
 
+                    word = temp.substring(0, i+1);
+                    words.push(word); // finding the index of spaces and substringing when a space is found. 
+                    System.out.println(word);
+                }
 
-
-
-
+                */
+            String tempSent = sentence.substring(0, index);
+            Scanner scan = new Scanner(tempSent);
+            while(scan.hasNext()){
+                words.push(scan.next());
+            } 
+            
+            }
+            
+            
+        
+        
     }
+
+    String total="";
+    while(words.size()>0){
+        total += words.pop(); //printing out the reversed sentence
+    }
+    return total;
 
     }
 }
