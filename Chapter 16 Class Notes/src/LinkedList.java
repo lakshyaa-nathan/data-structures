9406bf2a9712c273529c88e565b6bc8d3b2e13f3
lakshyaa-nathan <111480891+lakshyaa-nathan@users.noreply.github.com>
2023-10-16 13:18:20 -0500

    lesson

--- Chapter 16 Class Notes/src/LinkedList.java
+++ Chapter 16 Class Notes/src/LinkedList.java
@@ -8,12 +8,18 @@ import java.util.NoSuchElementException;
 */
 public class LinkedList
 {
+    // need a reference to the first node
+    // If the list is empty, first is null
 
+    private Node first;
 
     /**
         Constructs an empty linked list.
     */
 
+    public LinkedList(){
+        this.first = null;
+    }
 
 
 
@@ -22,14 +28,32 @@ public class LinkedList
         @return the first element in the linked list
     */
 
-
+    public Object getFirst(){
+        if(this.first == null){
+            throw new NoSuchElementException(); //gives an error if the first element is null
+        }
+        
+        return this.first.data;
+    }
 
 
     /**
         Removes the first element in the linked list.
         @return the removed element
     */
+     public Object removefirst(){
+        if(this.first == null){
+            throw new NoSuchElementException(); //gives an error if the first element is null
+        }
+        
+        Object element = this.first.data; //storing the object into a variable for future reference
 
+        this.first = this.first.next; //making the first object the second object
+
+        return element;
+
+
+    }
 
 
 
@@ -39,7 +63,15 @@ public class LinkedList
         @param element the element to add
     */
 
+    public void addFirst(Object element){
+        Node newNode = new Node();
 
+        newNode.data = element;
+
+        newNode.next= this.first;
+
+        this.first = newNode;
+    }
 
 
 
@@ -53,11 +85,24 @@ public class LinkedList
 
 
     //Class Node
+    // Node is static because it does not need access to anything in linked list
+    static class Node{
+        public Object data;
+        public Node next;
 
 
-    class LinkedListIterator //implements ListIterator
+    }
+
+
+    class LinkedListIterator implements ListIterator
     {
       //private data
+
+        // We need to know what node the iterator is at
+
+        private Node position;
+        private Node previous;
+        private boolean isAfterNext;
 
 
         /**
@@ -65,13 +110,37 @@ public class LinkedList
             of the linked list.
         */
 
+        public LinkedListIterator (){
+            position = null;
+            previous = null;
+             
+            isAfterNext=  false;
+
+        }
+
 
         /**
             Moves the iterator past the next element.
             @return the traversed element
         */
 
+        public Object next(){
+            previous = position;
 
+            isAfterNext = true;
+
+            //position is null when the iterator is first created.
+            if(position == null){
+                // at the end of the list
+                position = first; // loop back to the beginning of the list
+            }
+            else{
+                position = position.next;
+            }
+
+            return position.data;
+
+        }
 
 
 
